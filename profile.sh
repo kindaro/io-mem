95963c12bc139ed3290038bfcc474dda8d2b3e31
Ignat Insarov <kindaro@gmail.com>
2017-03-22 01:34:08 +0300

    Improve on profile script.

--- profile.sh
+++ profile.sh
@@ -1,13 +1,56 @@
-#! /bin/sh
+#! /bin/sh -ex
 
-stack clean                                                            &&
+proj="$(find *.cabal | rev | cut -d '.' -f 2- | rev)"
+
+while [ "${1:0:2}" == "--" ]
+do
+    opt="${1:2}"
+    [ "$opt" ] || break
+    case "$opt" in
+        ("clean") clean=yes ;;
+        ("build") build=yes ;;
+        ("time") time=yes;;
+        ("centre") [ "$centres" ] && centres="$centres $2" || centres="$2" ; shift ;;
+        ("review") review=yes ;;
+    esac
+    shift
+done
+
+if [ "$clean" ]
+then
+    rm *.hp *.prof *.ps *.aux && true
+    stack clean
+fi
+
+if [ "$build" ]
+then
     stack build                                     \
         --ghc-options "-rtsopts -auto-all -caf-all" \
         --executable-profiling                      \
-        --library-profiling                                            &&
+        --library-profiling
+fi
 
-    ./.stack-work/dist/x86_64-linux/Cabal-1.24.2.0/build/io-mem/io-mem \
-        +RTS -p -hc -RTS                                               &&
 
-    hp2ps io-mem.hp                                                    &&
-    zathura io-mem.ps
+if [ "$time" ]
+then
+    ./.stack-work/dist/x86_64-linux/Cabal-*/build/"$proj"/"$proj"        \
+        +RTS -p -hc -RTS
+    mv "$proj"{,-time}.hp
+fi
+
+[ "$centres" ] && echo "$centres" | tr ' ' '\n' | while read centre
+do
+    ./.stack-work/dist/x86_64-linux/Cabal-*/build/"$proj"/"$proj" \
+        +RTS -p -hd -hc"$centre" -RTS
+    mv "$proj"{,-"$centre"}.hp
+done
+
+if [ "$review" ]
+then
+    for file in "$proj"-*.hp
+    do
+        hp2ps "$file"
+    done
+
+    zathura "$proj"-*.ps 
+fi
