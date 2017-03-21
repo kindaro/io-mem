562c945059bea816ddb748b1c629adcf1d5b0aa3
Ignat Insarov <kindaro@gmail.com>
2017-03-22 02:15:24 +0300

    Adjust pure part to be more similar to IO part.

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,10 @@ import Lib
 main :: IO ()
 main = do
     print "Testing pure."
-    print $ hardPure 100000 100000
+    aPure <- return $ hardPure 100000 100000
+    bPure <- return $ hardPure 100000 100000
+    print (aPure, bPure)
+    cPure <- return $ hardPure 100000 100000
 
     print "Testing IO."
     a <- hardIO 100000 1
